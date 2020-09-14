d17bb4ff335c7d36930e98171a0766a2871ca118
spbnewslab <naumov@spbnews.ru>
2020-09-15 00:37:19 +0300

    done after review

--- posts/models.py
+++ posts/models.py
@@ -1,26 +1,42 @@
-from django.db import models
-# Create your models here.
 from django.contrib.auth import get_user_model
-
+from django.db import models
 
 User = get_user_model()
 
-class Group(models.Model):
-	title = models.CharField("Сообщество", max_length=200)
-	slug = models.SlugField("Ссылка (slug)", unique=True)
-	description = models.TextField("Описание")
-    
-	def __str__(self):
-		return self.title
 
-class Post(models.Model):
-    text = models.TextField("Текст")
-    pub_date = models.DateTimeField(
-            "Дата публикации", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-            related_name="posts", verbose_name='Автор')
-    group = models.ForeignKey(Group, on_delete=models.CASCADE,
-            blank=True, null=True, verbose_name='Сообщество')
+class Group(models.Model):
+    title = models.CharField('Сообщество', max_length=200)
+    slug = models.SlugField(
+        'Ссылка (slug)', unique=True, max_length=200
+    )
+    description = models.TextField(
+        'Описание', help_text='Краткое описание сообщества'
+    )
 
     def __str__(self):
-        return f'Пост № {self.pk}'
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField('Текст')
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='posts', verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        Group, on_delete=models.SET_NULL,
+        blank=True, null=True, related_name='posts',
+        verbose_name='Сообщество'
+    )
+
+    def posts_list(group=None):
+        posts_list = Post.objects.order_by('-pub_date')
+        if group is not None:
+            posts_list = posts_list.filter(group=group)
+        return posts_list
+
+    def __str__(self):
+        return f'{self.author} - {self.pub_date.date()} - {self.text[:20]} ...'
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
-from .models import Post, Group
+from .models import Group, Post
+
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
-    return render(request, "index.html", {"posts": latest})
+    latest = Post.posts_list()[:11]
+    return render(request, 'index.html', {'posts': latest})
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    return render(request, "group.html", {"group": group, "posts": posts})
+    posts = Post.posts_list(group)[:12]
+    return render(request, 'group.html', {'group': group, 'posts': posts})
